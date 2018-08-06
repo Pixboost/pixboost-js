b953e6bd8e3c4d21c49d7ae0b6c72ffe90e30f32
Dmitry Pokidov <dooman87@gmail.com>
2018-08-07 07:00:47 +1000

    * Building JS bundle with included libraries

--- release.js
+++ release.js
@@ -6,6 +6,9 @@ const rimraf = require('rimraf');
 
 const version = require('./package.json').version;
 const src = fs.readFileSync(`${__dirname}/pixboost.js`).toString('utf-8');
+const lozadSrc = fs.readFileSync(`${__dirname}/vendor/lozad.min.js`).toString('utf-8');
+const picturefillSrc = fs.readFileSync(`${__dirname}/vendor/picturefill.js`).toString('utf-8');
+
 const code = {
   'pixboost.js': `/* @preserve ${version} */\n${src}`
 };
@@ -24,6 +27,26 @@ if (minified.error) {
   process.exit(1);
 }
 
+const bundleCode = {
+  'picturefill.js': picturefillSrc,
+  'lozad.js': lozadSrc,
+  'pixboost.js': `/* @preserve ${version} */\n${src}`,
+};
+const bundleMinified = UglifyJS.minify(bundleCode, {
+  sourceMap: {
+    filename: 'pixboost.bundle.js',
+    url: 'pixboost.bundle.js.map'
+  },
+  output: {
+    comments: 'some'
+  }
+});
+
+if (bundleMinified.error) {
+  console.error(`Couldn't minify code: ${bundleMinified.error}`);
+  process.exit(2);
+}
+
 const distDir = `${__dirname}/dist`;
 if (fs.existsSync(distDir)) {
   rimraf.sync(distDir);
@@ -35,6 +58,11 @@ fs.writeFileSync(`${distDir}/pixboost.js`, code['pixboost.js']);
 fs.writeFileSync(`${distDir}/pixboost.min.js`, minified.code);
 fs.writeFileSync(`${distDir}/pixboost.js.map`, minified.map);
 
+fs.writeFileSync(`${distDir}/pixboost.bundle.min.js`, bundleMinified.code);
+fs.writeFileSync(`${distDir}/pixboost.bundle.js.map`, bundleMinified.map);
+
 fs.copyFileSync(`${__dirname}/vendor/intersection-observer.min.js`, `${distDir}/intersection-observer.min.js`);
 fs.copyFileSync(`${__dirname}/vendor/lozad.min.js`, `${distDir}/lozad.min.js`);
+fs.copyFileSync(`${__dirname}/vendor/lozad.js`, `${distDir}/lozad.js`);
 fs.copyFileSync(`${__dirname}/vendor/picturefill.min.js`, `${distDir}/picturefill.min.js`);
+fs.copyFileSync(`${__dirname}/vendor/picturefill.js`, `${distDir}/picturefill.js`);
