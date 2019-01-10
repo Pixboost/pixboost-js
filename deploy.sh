ecc5412eb094e64587761112578c2db48a662a57
Dmitry Pokidov <dooman87@gmail.com>
2019-01-11 07:13:07 +1100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ ${BRANCH} ne "master" ]]; then
+    echo "You must be on the master branch to deploy"
+    exit 1
+fi
+
+npm install
+npm test
+
+gsutil cp -a public-read -z js dist/* gs://pixboost-libs/new-libs/
