0caf92e7bf90f41e7acf501f6184fc9fe8d788e0
Dmitry Pokidov <dooman87@gmail.com>
2019-01-14 07:10:37 +1100

    Creating output artifacts with version

--- release.js
+++ release.js
@@ -9,43 +9,38 @@ const src = fs.readFileSync(`${__dirname}/pixboost.js`).toString('utf-8');
 const lozadSrc = fs.readFileSync(`${__dirname}/vendor/lozad.min.js`).toString('utf-8');
 const picturefillSrc = fs.readFileSync(`${__dirname}/vendor/picturefill.js`).toString('utf-8');
 
+const uglify = (code, sourceMapFilename) => {
+  const minified = UglifyJS.minify(code, {
+    sourceMap: {
+      filename: sourceMapFilename,
+      url: `${sourceMapFilename}.map`
+    },
+    output: {
+      comments: 'some'
+    }
+  });
+  if (minified.error) {
+    console.error(`Couldn't minify code: ${minified.error}`);
+    process.exit(1);
+  }
+
+  return minified;
+};
+
 const code = {
   'pixboost.js': `/* @preserve ${version} */\n${src}`
 };
-const minified = UglifyJS.minify(code, {
-  sourceMap: {
-    filename: 'pixboost.js',
-    url: 'pixboost.js.map'
-  },
-  output: {
-    comments: 'some'
-  }
-});
 
-if (minified.error) {
-  console.error(`Couldn't minify code: ${minified.error}`);
-  process.exit(1);
-}
+const minified = uglify(code, 'pixboost.js');
+const minifiedWithVersion = uglify(code, `pixboost-${version}.js`);
 
 const bundleCode = {
   'picturefill.js': picturefillSrc,
   'lozad.js': lozadSrc,
   'pixboost.js': `/* @preserve ${version} */\n${src}`,
 };
-const bundleMinified = UglifyJS.minify(bundleCode, {
-  sourceMap: {
-    filename: 'pixboost.bundle.js',
-    url: 'pixboost.bundle.js.map'
-  },
-  output: {
-    comments: 'some'
-  }
-});
-
-if (bundleMinified.error) {
-  console.error(`Couldn't minify code: ${bundleMinified.error}`);
-  process.exit(2);
-}
+const bundleMinified = uglify(bundleCode, 'pixboost.bundle.js');
+const bundleMinifiedWithVersion = uglify(bundleCode, `pixboost-${version}.bundle.js`);
 
 const distDir = `${__dirname}/dist`;
 if (fs.existsSync(distDir)) {
@@ -58,8 +53,15 @@ fs.writeFileSync(`${distDir}/pixboost.js`, code['pixboost.js']);
 fs.writeFileSync(`${distDir}/pixboost.min.js`, minified.code);
 fs.writeFileSync(`${distDir}/pixboost.js.map`, minified.map);
 
+fs.writeFileSync(`${distDir}/pixboost-${version}.js`, code['pixboost.js']);
+fs.writeFileSync(`${distDir}/pixboost-${version}.min.js`, minifiedWithVersion.code);
+fs.writeFileSync(`${distDir}/pixboost-${version}.js.map`, minifiedWithVersion.map);
+
 fs.writeFileSync(`${distDir}/pixboost.bundle.min.js`, bundleMinified.code);
 fs.writeFileSync(`${distDir}/pixboost.bundle.js.map`, bundleMinified.map);
+
+fs.writeFileSync(`${distDir}/pixboost-${version}.bundle.min.js`, bundleMinifiedWithVersion.code);
+fs.writeFileSync(`${distDir}/pixboost-${version}.bundle.js.map`, bundleMinifiedWithVersion.map);
 
 fs.copyFileSync(`${__dirname}/vendor/intersection-observer.min.js`, `${distDir}/intersection-observer.min.js`);
 fs.copyFileSync(`${__dirname}/vendor/lozad.min.js`, `${distDir}/lozad.min.js`);
